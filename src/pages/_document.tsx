222c04177ff6de8b1b03899e8cf2774da17841e1
draxya <68575901+draxya@users.noreply.github.com>
2022-03-27 16:31:44 +0300

    icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,7 +33,6 @@ class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
